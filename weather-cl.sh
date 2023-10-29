ed7027ed9f8315e59e5cfa5cb7df8ffcb0f865f6
Clunkbox Software <117487151+clunkbox@users.noreply.github.com>
2023-10-28 21:46:31 -0400

    View current local weather and forcast in terminal

---
+++ weather-cl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# View local weather and forcast from wttr.in 
+# in your terminal. (Maximize window)
+# VPN may show another location's weather
+
+
+# Kenny Seiler
+# 12/12/2020
+
+clear
+curl wttr.in
+echo "========================="
+echo "PRESS ENTER WHEN FINISHED"
+read input
+clear
+
+
+
+
