3ef3f30dc47c1cfa057291e93b88b03de6898e1a
ClunkBox Software <117487151+clunkbox@users.noreply.github.com>
2024-01-10 20:52:00 -0500

    Sets Headphone volume to 100% on a Panosonic CF-C2
    This script is a work around. Mint Linux does not configure the audio device properly. So when you set the headphone speakers to 100%
    it actually sets the internal speakers to 100%.

---
+++ set-volume.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is a work around to set the 
+# the headphone volume to 100 Which makes 
+# internal speakers play on a Panonsonic CF-C2  
+# Kenny Seiler
+# 07/04/2023
+
+amixer sset Headphone 100%
+
